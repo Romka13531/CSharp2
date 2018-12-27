2e87d54201b10c5087aa23184029e6873c793d2e
Romka13531 <45291876+Romka13531@users.noreply.github.com>
2018-12-27 14:14:18 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        MyString.MyString str3 = new MyString.MyString("Hello ");
+        MyString.MyString str = new MyString.MyString("Hello ");
+        MyString.MyString str1 = new MyString.MyString("World");
+        MyString.MyString str2 = new MyString.MyString("!");
+        //str = str.Concat(str1);
+        //str = str.Concat(str2);
+        Console.WriteLine(str1.IndexOf('r'));
+        str = str + str1 + str2;
+        Console.WriteLine(str);
+        Console.ReadLine();
+    }
+}
